c1eeff5835ada6792762beb65139da5f163789db
VINICIUS\vinic <viniciusmoreiradamatta@gmail.com>
2022-05-30 21:05:18 -0300

    Poc RabbitMq

---
+++ src/AppConsumer1/Program.cs
@@ -0,0 +1,37 @@
+﻿using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+
+Console.WriteLine("Hello, World!");
+
+ConnectionFactory _connectionFactory = new()
+{
+    HostName = "localhost",
+    Port = 5672,
+    UserName = "usuarioMaster",
+    Password = "s3nh40k",
+    VirtualHost = "main",
+    DispatchConsumersAsync = true
+};
+
+using var connection = _connectionFactory.CreateConnection();
+var model = connection.CreateModel();
+
+model.QueueDeclare("pocExchange_consumer1_queue", true, false, false);
+model.QueueBind("pocExchange_consumer1_queue", "pocExchange", "test.#", null);
+
+var consumer = new AsyncEventingBasicConsumer(model);
+
+model.BasicQos(0, 1, false);
+
+consumer.Received += async (_, eventArgs) =>
+ {
+     await Task.Delay(2000);
+ };
+
+model.BasicConsume(queue: "pocExchange_consumer1_queue", autoAck: false, consumer: consumer);
+
+while (true)
+{
+    await Task.Delay(1000);
+    Console.WriteLine("runnign");
+}
---
+++ src/AppPublisher/Program.cs
@@ -0,0 +1,51 @@
+﻿using System.Text;
+using RabbitMQ.Client;
+
+Console.WriteLine("Hello, World!");
+
+ConnectionFactory _connectionFactory = new()
+{
+    HostName = "localhost",
+    Port = 5672,
+    UserName = "usuarioMaster",
+    Password = "s3nh40k",
+    VirtualHost = "main",
+    DispatchConsumersAsync = true
+};
+
+using var connection = _connectionFactory.CreateConnection();
+var model = connection.CreateModel();
+
+model.ExchangeDeclare($"pocExchange", "topic", true, false, null);
+
+int i = 0;
+
+while (true)
+{
+    model.BasicPublish("pocExchange", "");
+    model.ConfirmSelect();
+
+    var basicProperties = model.CreateBasicProperties();
+
+    basicProperties.MessageId = Guid.NewGuid().ToString("D");
+
+    basicProperties.DeliveryMode = 2;
+
+    basicProperties.Headers = new Dictionary<string, object>()
+            {
+                {"content-type","application/json" }
+            };
+
+    var data = new { Ok = true, Value = i };
+
+    var body = Encoding.UTF8.GetBytes(System.Text.Json.JsonSerializer.Serialize(data));
+
+    model.BasicPublish(exchange: "pocExchange",
+                        routingKey: "test.app.add",
+                        basicProperties: basicProperties,
+                        body: body);
+
+    i++;
+
+    await Task.Delay(1000);
+}
